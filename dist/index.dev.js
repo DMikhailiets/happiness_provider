19b6886ae3ee3a50e1350ca81e27572c58c7f363
DMikhailiets <dmikhailiets21@gmail.com>
2020-10-06 16:30:59 +0300

    modified goods block

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -4,3 +4,29 @@ var burgerIsActive = function burgerIsActive() {
   document.getElementById('Header').classList.toggle('isActive');
   document.getElementById('root').classList.toggle('lock');
 };
+
+var descriptionSplit = function descriptionSplit() {
+  var n = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 400;
+  var elementList = document.querySelectorAll('.goodDescription');
+  elementList.forEach(function (description) {
+    if (description.innerHTML.length > n) {
+      var text = description.innerHTML.slice(0, n) + '...';
+      description.replaceWith(document.createTextNode(text));
+    }
+  });
+};
+
+descriptionSplit();
+
+var titleSplit = function titleSplit() {
+  var n = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 30;
+  var elementsList = document.querySelectorAll('.goodTitle');
+  elementsList.forEach(function (title) {
+    if (title.innerHTML.length > n) {
+      var text = title.innerHTML.slice(0, n) + '...';
+      title.replaceWith(document.createTextNode(text));
+    }
+  });
+};
+
+titleSplit();
--- js/index.js
+++ js/index.js
@@ -2,3 +2,27 @@ let burgerIsActive = () => {
     document.getElementById('Header').classList.toggle('isActive')
     document.getElementById('root').classList.toggle('lock')
 }
+
+let descriptionSplit = (n = 400) => {
+    let elementList = document.querySelectorAll('.goodDescription')
+    elementList.forEach((description) => {
+        if(description.innerHTML.length > n) {
+            let text = description.innerHTML.slice(0, n) + '...'
+            description.replaceWith( document.createTextNode(text))
+        }
+    })
+}
+
+descriptionSplit()
+
+let titleSplit = (n = 30) => {
+    let elementsList = document.querySelectorAll('.goodTitle')
+    elementsList.forEach((title) => {
+        if(title.innerHTML.length > n) {
+            let text = title.innerHTML.slice(0, n) + '...'
+            title.replaceWith(document.createTextNode(text))
+        }
+    })
+}
+
+titleSplit()
